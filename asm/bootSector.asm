b454f6769dbd27f5486b24cab29c578abb6913ba
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-02-23 15:25:30 +0000

    Update bootSector.asm

--- asm/bootSector.asm
+++ asm/bootSector.asm
@@ -6,11 +6,14 @@ DATA_SEG equ GDT_Data - GDT_Start
 
 cli			; pauses all interrupts	
 mov eax, cr0		
-or eax, 1
+or eax, 1 
 mov cr0, eax		; sets bit flag to allow protected mode to start 
-lgdt[GDT_Desc]	
+lgdt[GDT_Desc]		; loads GDT
 jmp CODE_SEG:start_protected_mode
 jmp $
+
+; -------------
+; GDT table definition
 
 GDT_Start:
 	GDT_Null:
@@ -35,12 +38,20 @@ GDT_End:
 GDT_Desc:
 	dw GDT_End - GDT_Start - 1
 	dd GDT_Start
-
+;--------------
 
 
 BITS 32
 start_protected_mode:
 	; video mem 0xb8000
+	; BEN YOU FORGOT TO SET THE REGISTERS WHAT ARE YOU DOING
+	; NOW YOU CANT EVEN REMEMBER WHICH REGISTERS NEED TO BE SET
+	mov eax, 0
+	mov sp, eax
+	mov bp, eax
+	mov si, eax
+	
+
 	mov al, 'A'
 	mov ah, 0x0f
 	mov [0xb8000], ax
